20949915835dfaf745e5d3c4c44ccea0a75f5565
APSDIGIT\vballut <vincentballut@outlook.fr>
2023-03-06 11:38:08 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'englishWords',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
